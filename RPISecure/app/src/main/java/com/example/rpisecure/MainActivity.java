f9b519f082c29b41c383f7eee1edf4819173c0ae
Jason <34655994+HackerVis@users.noreply.github.com>
2019-05-12 21:41:27 -0600

    Streaming Work (Errors Involved)
    Worked on raspberry pi streaming!

--- RPISecure/app/src/main/java/com/example/rpisecure/MainActivity.java
+++ RPISecure/app/src/main/java/com/example/rpisecure/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.rpisecure;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.annotation.SuppressLint;
+        import android.app.ProgressDialog;
+        import android.media.MediaPlayer;
+        import android.net.Uri;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.webkit.WebView;
+        import android.widget.ImageButton;
+        import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ProgressDialog dialog;
+    WebView videoView;
+    ImageButton btnPlay;
 
+    String videoURL = "https://192.168.137.216:8081/";
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        videoView = (WebView) findViewById(R.id.videoView);
+        btnPlay = (ImageButton) findViewById(R.id.btn_play);
+        btnPlay.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        dialog = new ProgressDialog(MainActivity.this);
+        dialog.setMessage("Please wait....");
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.show();
+        WebView videoView = (WebView) findViewById(R.id.videoView);
+        videoView.loadUrl(videoURL);
+
     }
 }
+
+
