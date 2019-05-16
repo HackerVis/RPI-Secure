2754dd8358fcfdec5331b909b11f2c47c68c717b
Jason <34655994+HackerVis@users.noreply.github.com>
2019-05-16 13:27:40 -0600

    Updated Login Capabilities

--- RPISecure/app/src/main/java/com/example/rpisecure/MainActivity.java
+++ RPISecure/app/src/main/java/com/example/rpisecure/MainActivity.java
@@ -1,33 +1,18 @@
 package com.example.rpisecure;
 
-        import android.annotation.SuppressLint;
         import android.app.ProgressDialog;
-        import android.graphics.Bitmap;
-        import android.graphics.Canvas;
-        import android.graphics.Paint;
-        import android.graphics.Picture;
-        import android.media.MediaPlayer;
-        import android.net.Uri;
-        import android.os.Environment;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.webkit.WebView;
-        import android.webkit.WebViewClient;
-        import android.widget.ImageButton;
-        import android.widget.VideoView;
-
-        import java.io.File;
-        import java.io.FileOutputStream;
-        import java.io.OutputStream;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     ProgressDialog dialog;
     WebView videoView;
     ImageButton btnPlay;
 
-    String videoURL = "http://192.168.137.216:8081/";
-    @SuppressLint("WrongViewCast")
+    String videoURL = "http://10.23.25.246:8081/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,58 +32,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.webview);
-
-        webView = (WebView) findViewById(R.id.webView1);
-        webView.loadUrl("http://developer.android.com/reference/packages.html");
-//      webView.loadUrl("http://developer.android.com/training/basics/firstapp/creating-project.html");
-
-        webView.setWebViewClient(new WebViewClient() {
-
-            public void onPageFinished(WebView view, String url) {
-                // do your stuff here
-                webView.measure(MeasureSpec.makeMeasureSpec(
-                        MeasureSpec.UNSPECIFIED, MeasureSpec.UNSPECIFIED),
-                        MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
-                webView.layout(0, 0, webView.getMeasuredWidth(),
-                        webView.getMeasuredHeight());
-                webView.setDrawingCacheEnabled(true);
-                webView.buildDrawingCache();
-                Bitmap bm = Bitmap.createBitmap(webView.getMeasuredWidth(),
-                        webView.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
-
-                Canvas bigcanvas = new Canvas(bm);
-                Paint paint = new Paint();
-                int iHeight = bm.getHeight();
-                bigcanvas.drawBitmap(bm, 0, iHeight, paint);
-                webView.draw(bigcanvas);
-                System.out.println("1111111111111111111111="
-                        + bigcanvas.getWidth());
-                System.out.println("22222222222222222222222="
-                        + bigcanvas.getHeight());
-
-                if (bm != null) {
-                    try {
-                        String path = Environment.getExternalStorageDirectory()
-                                .toString();
-                        OutputStream fOut = null;
-                        File file = new File(path, "/aaaa.png");
-                        fOut = new FileOutputStream(file);
-
-                        bm.compress(Bitmap.CompressFormat.PNG, 50, fOut);
-                        fOut.flush();
-                        fOut.close();
-                        bm.recycle();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-    }
-}
-
     }
 }
 
