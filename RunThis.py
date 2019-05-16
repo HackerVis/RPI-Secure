28e6140c4d0180d7f4dd2832e9f8199dd77d15e4
HackerVis <21firjas@student.mcpsmt.org>
2019-05-16 01:12:54 +0100

    pi commit

--- RunThis.py
+++ RunThis.py
@@ -1,5 +1,9 @@
 import os
 
-os.system("git add /home/pi/Desktop/RPISecure")
-os.system("git commit")
-os.system("git push") 
+os.system("cd /home/pi/Desktop/RPISecure")
+os.system("git add RPISafeSecure/")
+os.system("git config user.email" + "'" + "21firjas@student.mcpsmt.org" + "'")
+os.system("git config user.name" + "'" +"HackerVis"+ "'")
+os.system("git config user.password" + "'" + "durnin12" +"'")
+os.system("git commit -m" + "'" + "Updated Security Cam" + "'")
+os.system("git push --set-upstream origin master") 
