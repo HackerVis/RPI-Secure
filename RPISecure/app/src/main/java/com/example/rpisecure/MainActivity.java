be16337f188927f49286f059a7917fe6b063251a
Jason <34655994+HackerVis@users.noreply.github.com>
2019-05-30 13:50:38 -0600

    Status Main Finish
    Finished core concepts of the app

--- RPISecure/app/src/main/java/com/example/rpisecure/MainActivity.java
+++ RPISecure/app/src/main/java/com/example/rpisecure/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
@@ -23,19 +24,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     ImageButton btnPlay;
     ImageButton ipchange;
     EditText etIp;
+    Button btnUser;
+    Button btnSubmit;
     EditText note;
+    EditText newUse;
+    EditText newPass;
     int monitor = 0;
     int monitor2 = 0;
     int monitor4 = 0;
     Context context;
+    Boolean on;
     String ipanew;
     String oldIp;
     ImageButton btnNotes;
     String notes = "";
+    ImageButton btnLogout;
     BackendlessUser currentUser;
     String noteOld;
     ImageButton set;
     int monitor3 = 0;
+    String store1 = "";
+    String store2 = "";
 
 
     private final String TAG = this.getClass().getSimpleName();
@@ -44,6 +53,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
         context = getApplicationContext();
         setContentView(R.layout.activity_main);
@@ -54,8 +64,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ImageButton set = findViewById(R.id.btn_set);
         btnPlay = findViewById(R.id.btn_recordings);
         ipanew = ip;
-
-
+        btnUser = findViewById(R.id.userconfig);
+        btnSubmit = findViewById(R.id.btn_submit);
+        newUse = findViewById(R.id.edit_user);
+        newPass = findViewById(R.id.edit_password);
+        on=false;
+        String email = currentUser.getEmail().toString();
+        newUse.setText(email);
+        store2 = newUse.getText().toString();
+        newPass.setText("");
+        btnLogout = findViewById(R.id.logout);
 
         videoURL = "http://" + ip + "/";
         etIp = findViewById(R.id.newip);
@@ -100,6 +118,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void onClick(View v) {
                 RecordButtonClicked();
+            }
+        });
+        btnUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UserSetupButtonClicked();
+            }
+        });
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SubmitButtonClicked();
+            }
+        });
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LogoutButtonClicked();
             }
         });
     }
@@ -208,6 +244,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             btnPlay.setVisibility(View.VISIBLE);
             etIp.setVisibility(View.GONE);
             videoView.setVisibility(View.VISIBLE);
+            //settings
+            btnUser.setVisibility(View.GONE);
+            if(on){
+                newPass.setVisibility(View.GONE);
+                newUse.setVisibility(View.GONE);
+                btnSubmit.setVisibility(View.GONE);
+                on = false;
+            }
+            btnLogout.setVisibility(View.GONE);
             monitor3 = 1;
         }
         // Display setting options
@@ -220,6 +265,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             btnPlay.setVisibility(View.GONE);
             btnNotes.setVisibility(View.GONE);
             videoView.setVisibility(View.GONE);
+            btnLogout.setVisibility(View.VISIBLE);
+            //settings
+            btnUser.setVisibility(View.VISIBLE);
+            if(on){
+                newPass.setVisibility(View.GONE);
+                newUse.setVisibility(View.GONE);
+                btnSubmit.setVisibility(View.GONE);
+                on = false;
+            }
             monitor3 = 0;
         }
     }
@@ -231,10 +285,112 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             monitor4 = 1;
         }
         else if (monitor4 == 1){
-            videoURL = "http://" + etIp.getText().toString() + "/";
+            oldIp = etIp.getText().toString();
+            videoURL = "http://" + oldIp + "/";
+            WebView videoView = (WebView) findViewById(R.id.videoView);
             videoView.loadUrl(videoURL);
             monitor4 = 0;
         }
+
+    }
+    private void UserSetupButtonClicked() {
+        btnUser.setVisibility(View.GONE);
+        newPass.setVisibility(View.VISIBLE);
+        newUse.setVisibility(View.VISIBLE);
+        btnSubmit.setVisibility(View.VISIBLE);
+        on = true;
+    }
+    private void SubmitButtonClicked() {
+        BackendlessUser user = currentUser;
+        String password = currentUser.getPassword();
+        newUse = findViewById(R.id.edit_user);
+        newPass = findViewById(R.id.edit_password);
+        store2 = newUse.getText().toString();
+        newPass.setText("");
+        store1 = newUse.getText().toString();
+        store2 = newPass.getText().toString();
+
+
+        if(store1 != ""){
+            if(newPass.equals("") || newPass.equals(password)){
+                btnNotes.setVisibility(View.GONE);
+                videoView.setVisibility(View.GONE);
+                ipchange.setVisibility(View.GONE);
+                etIp.setVisibility(View.GONE);
+                ipchange.setVisibility(View.GONE);
+                btnPlay.setVisibility(View.GONE);
+                btnNotes.setVisibility(View.GONE);
+                //settings
+                btnUser.setVisibility(View.VISIBLE);
+                newPass.setVisibility(View.GONE);
+                newUse.setVisibility(View.GONE);
+                btnSubmit.setVisibility(View.GONE);
+                on = false;
+            }
+            else{
+                user.setEmail(store1);
+                user.setPassword(store2);
+                Backendless.UserService.update( user, new AsyncCallback<BackendlessUser>()
+                {
+                    @Override
+                    public void handleResponse( BackendlessUser backendlessUser )
+                    {
+                        btnNotes.setVisibility(View.GONE);
+                        videoView.setVisibility(View.GONE);
+                        ipchange.setVisibility(View.GONE);
+                        etIp.setVisibility(View.GONE);
+                        ipchange.setVisibility(View.GONE);
+                        btnPlay.setVisibility(View.GONE);
+                        btnNotes.setVisibility(View.GONE);
+                        //settings
+                        btnUser.setVisibility(View.VISIBLE);
+                        newPass.setVisibility(View.GONE);
+                        newUse.setVisibility(View.GONE);
+                        btnSubmit.setVisibility(View.GONE);
+                        on = false;
+
+                    }
+
+                    @Override
+                    public void handleFault( BackendlessFault backendlessFault )
+                    {
+
+                    }
+                } );
+            }
+        }
+        else {
+            btnNotes.setVisibility(View.GONE);
+            videoView.setVisibility(View.GONE);
+            ipchange.setVisibility(View.GONE);
+            etIp.setVisibility(View.GONE);
+            ipchange.setVisibility(View.GONE);
+            btnPlay.setVisibility(View.GONE);
+            btnNotes.setVisibility(View.GONE);
+            //settings
+            btnUser.setVisibility(View.VISIBLE);
+            newPass.setVisibility(View.GONE);
+            newUse.setVisibility(View.GONE);
+            btnSubmit.setVisibility(View.GONE);
+            on = false;
+        }
+    }
+    private void LogoutButtonClicked() {
+        Backendless.UserService.logout( new AsyncCallback<Void>()
+        {
+            public void handleResponse( Void response )
+            {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+
+            public void handleFault( BackendlessFault fault )
+            {
+                // something went wrong and logout failed, to get the error code call fault.getCode()
+            }
+        });
+
+
 
     }
 
